10e920fbbf7a77175148f2e79bfdfa88d4892eab
s0net <steve.louies.alappat@gmail.com>
2022-02-02 12:40:49 +0530

    changes

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-const thign = require("./module")
-const somthing = require("http")
+const {names, domains, animals, birds} = require("./list")
+
+const pick = (arr) => {
+    const pic = Math.floor(Math.random() * arr.length)
+    return arr[pic]
+};
+
+const username = pick(names);
+const domain = pick(domains);
+const email = `${username}@${domain}`;
+const age = Math.floor(Math.random() * (80 - 10 + 1)) - 1;
+const animal = pick(animals)
+const bird = pick(birds)
+
+const list = {
+    username: username,
+    domain: domain,
+    email: email,
+    age: age,
+    animal: animal,
+    bird: bird
+};
+
+console.log(list);
---
+++ list.js
@@ -0,0 +1,75 @@
+const names = [
+    "Steve",
+    "John",
+    "Jimmy",
+    "Bob",
+    "Ron",
+    "Harry",
+    "Ann",
+    "Mel",
+    "Alice",
+    "Jack",
+    "Sam",
+    "Maria",
+    "Mark",
+    "Adele",
+    "Billy",
+    "Calvin",
+    "jay",
+    "Christina"
+];
+
+const domains = [
+    "google.com",
+    "yahoo.com",
+    "nothing.com",
+    "bit.net",
+    "coin.com",
+    "flutter.org",
+    "w3schools.com",
+    "w3.org",
+    "microsoft.com",
+    "vscode.com",
+    "github.com"
+];
+
+const animals = [
+    "spider",
+    "lion",
+    "tiger",
+    "lizard",
+    "panda",
+    "deer",
+    "wolf",
+    "forg",
+];
+
+const birds = [
+    "crow",
+    "eagles",
+    "swan",
+    "hummingbird",
+    "kiwi",
+    "moa",
+    "falcon",
+    "dodo",
+    "cuckoo",
+    "vulture",
+    "barbet",
+    "pigeon",
+    "peacock",
+    "parrot",
+    "sparrow",
+    "dove",
+    "goose",
+    "ostrich",
+    "turkey",
+    "jay",
+    "parakeets",
+    "owl"
+];
+
+exports.names = names;
+exports.birds = birds;
+exports.animals = animals;
+exports.domains = domains
